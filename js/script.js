3e4e2db8d1664c70e944efcbdbe48a63845e2bb3
oi-wso <70068334+oi-wso@users.noreply.github.com>
2020-08-31 17:01:05 +0300

    add a portfolio

---
+++ js/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: true,
+		dots: true,
+		// adaptiveHeight: true,
+		slidesToShow: 3,
+  slidesToScroll: 1,
+  	responsive: [
+  		{
+  			breakpoint: 993,
+  			settings: {
+  				arrows: false,
+  				slidesToShow: 2
+  			}
+  		},
+  		{
+  			breakpoint: 768,
+  			settings: {
+  				arrows: false,
+  				slidesToShow: 1
+  			}
+  		}
+  	]
+
+	});
+
+  function ibg(){
+  $.each($('.ibg'), function(index, val) {
+    if($(this).find('img').length>0){
+      $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+    }
+  });
+}
+ibg();
+
+  $('.goto').click(function() {
+    var el=$(this).attr('href').replace('#','');
+    var offset=0;
+  $('body,html').animate({scrollTop:$('.'+el).offset().top+offset},500, function() {});
+
+  return false;
+});
+
+
+
+$(window).resize(function(event) {
+	var h=$(window).outerHeight();
+	$('.mainblock').css('min-height',h);
+});
+
+$(window).scroll(function(event) {
+	var s=0-$(this).scrollTop()/2;
+	$('.mainblock__image').css('transform','translate3d(0, '+s+'px, 0)');
+});
+
+ $('.popup__open-btn').click(function(event){
+    $('.popup').fadeIn(400);
+    $('body').addClass('lock');
+ });
+  $('.popup__close-btn').on('click', function(event){
+    event.preventDefault();
+    $('.popup').fadeOut(400);
+    $('body').removeClass('lock');
+   });
+
+});
