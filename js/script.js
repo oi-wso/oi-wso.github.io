f477bbcb1f59b63778f97f67a5be418ea8534528
oi-wso <70068334+oi-wso@users.noreply.github.com>
2020-09-08 19:55:19 +0300

    add email post form
    add email post form

--- js/script.js
+++ js/script.js
@@ -63,4 +63,21 @@ $(window).scroll(function(event) {
     $('body').removeClass('lock');
    });
 
+  //E-mail Ajax Send
+  $("form").submit(function() { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      alert("Спасибо за заявку!");
+      setTimeout(function() {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
 });
